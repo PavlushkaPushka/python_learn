c3306660f606ae5d838c1d15abfd295180e0addd
Pavel <parabellum.gmm@gmail.com>
2024-02-28 15:05:08 +0500

    4 lessons

---
+++ lesson_1.py
@@ -0,0 +1,9 @@
+############# Принты, устройство объектов
+
+
+print("Hello world!", 2, True)
+print(print)
+print(dir())
+print(dir(__builtins__))
+
+
---
+++ lesson_2.py
@@ -0,0 +1,13 @@
+# Методы строк
+
+
+# input("Enter your name : ")
+name = input("Enter your name : ")
+# При  этом значение переменной name не изменилось
+print(name, name.capitalize(), name.upper(), name.count('n'), sep='\n\n', )
+
+# print(name.capitalize()) #При  этом значение переменной name не изменилось
+
+# print(name.upper())
+
+print(dir('Hello'))
